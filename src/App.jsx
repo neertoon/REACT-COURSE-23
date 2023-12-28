b3eab03d6dd0da132f50c7df5ad7819647196fdd
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2023-12-28 20:45:49 +0100

    STATE - ładowanie zawartości na podstawie tablicy danych

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import { CORE_CONCEPTS } from './data.js';
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
+import { EXAMPLES } from './data.js';
 function App() {
-    const [ selectedTopic, setSelectedTopic ] = useState("Please select a tab");
+    const [ selectedTopic, setSelectedTopic ] = useState("components");
 
     function handleSelect(element) {
         setSelectedTopic(element);
@@ -35,10 +36,14 @@ function App() {
                         <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
                         <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
                     </menu>
-                    <div className="tab-content">
-                        <p>
-                            {selectedTopic}
-                        </p>
+                    <div id="tab-content">
+                        <h3>{EXAMPLES[selectedTopic].title}</h3>
+                        <p>{EXAMPLES[selectedTopic].description}</p>
+                        <pre>
+                            <code>
+                                {EXAMPLES[selectedTopic].code}
+                            </code>
+                        </pre>
                     </div>
                 </section>
             </main>
