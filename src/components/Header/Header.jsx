3b5219ded3212ece62bfe59912280c93439bb55c
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2023-12-28 15:23:12 +0100

    STYLE - style osobno dla komponentu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import reactImg from "../assets/react-core-concepts.png";
+import reactImg from "../../assets/react-core-concepts.png";
+import "./Header.css";
 
 const createDescription = ['Fundamental', 'Crucial', 'Core'];
 
