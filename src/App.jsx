050988d46d7be36c7b1680d45b0ee15b81eaa559
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2023-12-28 13:44:15 +0100

    Dynamiczne wartości/dane w komponencie

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+const createDescription = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+    return Math.floor(Math.random() * (max));
+}
+
 function Header() {
+    const description = createDescription[genRandomInt(3)];
     return (
         <header>
             <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
             <h1>React Essentials</h1>
             <p>
-                Fundamental React concepts you will need for almost any app you are
+                {description} React concepts you will need for almost any app you are
                 going to build!
             </p>
         </header>
