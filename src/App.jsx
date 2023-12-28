eef401df8697953cbe47869675ab817a64b23410
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2023-12-28 14:02:26 +0100

    PROPS - ustawianie i odczytywanie w komponencie

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
 
 const createDescription = ['Fundamental', 'Crucial', 'Core'];
 
@@ -20,15 +21,37 @@ function Header() {
     );
 }
 
+function CoreConcept(props) {
+    return (
+        <li>
+            <img src={props.image} alt={props.title}/>
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+        </li>
+    );
+}
+
 function App() {
     return (
         <div>
             <Header></Header>
             <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept
+                            title="Components"
+                            description="The core UI building block"
+                            image={componentsImg}></CoreConcept>
+                        <CoreConcept></CoreConcept>
+                        <CoreConcept></CoreConcept>
+                        <CoreConcept></CoreConcept>
+                    </ul>
+                </section>
                 <h2>Time to get started!</h2>
             </main>
         </div>
-  );
+    );
 }
 
 export default App;
