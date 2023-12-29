d7bcc927217719b74f4ea266759a3d737f7afc2a
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2023-12-29 19:45:51 +0100

    JSX - FRAGMENT bo nie chcemy mieć diva nad wszystkicm - sytax sugar trochę :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 
 import { CORE_CONCEPTS } from './data.js';
 import Header from "./components/Header/Header";
@@ -29,7 +29,7 @@ function App() {
     }
 
     return (
-        <div>
+        <Fragment>
             <Header></Header>
             <main>
                 <section id="core-concepts">
@@ -49,7 +49,7 @@ function App() {
                     {tabContent}
                 </section>
             </main>
-        </div>
+        </Fragment>
     );
 }
 
