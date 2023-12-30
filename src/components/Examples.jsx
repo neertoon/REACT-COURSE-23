3679c6d62ddfcf9bcf3237c1403656bc322f1899
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2023-12-30 16:20:42 +0100

    KOMPONENTY - przekazywanie przez atrybut do innego komponentu

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import TabButton from "./TabButton";
 import {EXAMPLES} from "../data";
 import Section from "./Section";
+import Tabs from "./Tabs";
 
 export default function Examples() {
     const [ selectedTopic, setSelectedTopic ] = useState();
@@ -28,13 +29,16 @@ export default function Examples() {
 
     return (
         <Section title="Examples" id="examples">
-            <menu>
+            <Tabs buttons={
+                <>
                 <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Component</TabButton>
                 <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
                 <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
                 <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
-            </menu>
-            {tabContent}
+                </>
+            }>
+                {tabContent}
+            </Tabs>
         </Section>
     );
 }
